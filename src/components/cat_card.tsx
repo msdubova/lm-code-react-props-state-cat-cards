aeb5a08cd03264a60123c918a59df1e6ea38b446
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-08-30 22:59:26 +0100

    Activity 5

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -5,12 +5,15 @@ interface CatCardProps {
   birthYear: number;
 }
 
-const CatCard: React.FC<CatCardProps> = (props) => (
-  <div className="card">
-    <h3 className="card__text card__header">Mr Gwumpy</h3>
-    <p className="card__text">Species: Cat</p>
-    <p className="card__text">Favourite Food(s): Caviar</p>
-    <p className="card__text">Birth Year: 1984</p>
-  </div>
-);
+const CatCard: React.FC<CatCardProps> = (props) => {
+  // console.log("CatCard to App, come in App? Do you read me?", props)
+  return (
+    <div className="card">
+      <h3 className="card__text card__header">{props.name}</h3>
+      <p className="card__text">Species: {props.species}</p>
+      <p className="card__text">Favourite Food(s): {props.favFoods}</p>
+      <p className="card__text">Birth Year: {props.birthYear}</p>
+    </div>
+  );
+};
 export default CatCard;
